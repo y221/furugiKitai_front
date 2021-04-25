cad96b7ac10ab80988f8d6e8cebfe1329ad5810c
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2021-04-25 19:09:34 +0900

    ユーザー詳細ページの実装

---
+++ components/molecules/UserProfile.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <ul class="mb-5">
+        <li
+          v-for="item in items"
+          :key="item.title"
+          class="profile-list"
+        >
+          <span
+            v-text="item.title"
+            class="profile-title"></span>
+          <span
+            v-text="item.text"
+            class="profile-text"></span>
+        </li>
+    </ul>
+    <v-divider></v-divider>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: '好きな古着',
+          text: 'levisとかトレーナーとかアメリカの学生みたいな格好が好き'
+        },
+        {
+          title: 'プロフィール',
+          text: '古着屋には詳しくないけど古着屋に行くのは好き'
+        },
+        {
+          title: 'Instagram',
+          text: 'http://instagram/xxxxxxxxx'
+        },
+        {
+          title: 'リンク',
+          text: 'http://xxxxxxxxxxxxxxxxxxx'
+        },
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+.profile-list{
+  display: flex;
+  font-size: 14px;
+  margin-bottom: 10px;
+}
+.profile-title{
+  display: block;
+  font-size: 14px;
+  font-weight: 700;
+  color: #888;
+  width: 120px;
+}
+.profile-text{
+  width: calc(100% - 120px);
+}
+</style>
---
+++ components/molecules/UserProfileTop.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="user-header">
+      <div class="user-header_header">
+        <v-btn
+          text
+          to="/"
+        >
+          プロフィール編集
+        </v-btn>
+      </div>
+      <div class="user-header_content">
+        <v-avatar class="user-avatar" size="80px">
+          <v-img
+            :alt="user"
+            :src="src"
+          ></v-img>
+        </v-avatar>
+        <p class="text-h5 ml-10 mb-0 font-weight-bold user-name">{{ user }}</p>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: String,
+      required: true,
+    },
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.user-header_header {
+  display: flex;
+  justify-content: flex-end;
+}
+.user-header_content {
+  padding: 0px 0px 24px 24px;
+  display: flex;
+}
+.user-avatar {
+  width: 120px;
+}
+.user-name {
+  display: flex;
+  align-items: center;
+}
+</style>
