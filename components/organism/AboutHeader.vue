c9dc367de80830c96cda65f9634813fb68d87d54
pmhr18 <tmh6.pm18@gmail.com>
2021-05-05 13:38:20 +0900

    アバター追加してみる アバター間の余白入れたい、画像ファイルを入れたい

--- components/organism/AboutHeader.vue
+++ components/organism/AboutHeader.vue
@@ -13,36 +13,55 @@
       </div> 
       <div class="AboutAbout">
         <div class="mt-5">
-        <v-col align="center">
-          <div class="text-h5 font-weight-bold">古着屋が見つかる検索サイト</div>
-            <div class="my-10">
+          <v-col align="center">
+          <div class="text-h4 font-weight-bold">古着屋が見つかる検索サイト</div>
+          </v-col>
+            <v-col align="left">  
+            <div class="my-10 text-h5">
               <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
               <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
               <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
             </div>
-        </v-col>  
+            </v-col>
         </div>
       </div>
       <div class="AboutConcept">
         <div class="mt-5">
-        <v-col align="center">
-          <div class="text-h5 font-weight-bold">フルギキタイへの想い</div>
-            <div class="my-10">
-              <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
-              <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
-              <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
+          <v-col align="center">
+          <div class="text-h4 font-weight-bold">フルギキタイへの想い</div>
+          </v-col>
+            <v-col align="left">
+            <div class="my-10 text-h5">
+              <div>洋服は、僕らが身につけるものの中で、一番日常的で、僕らの気持ちの変化にも敏感なものです。</div>
+              <div>そんな洋服、古着を仕事にしている人たちが、好きなことに対して情熱を持って働いている姿を見て、棒らにもなにかできることがないかと考えました。</div>
+              <div>それぞれの好きを追い求めて、ぼくたちはフルギキタイを立ち上げました。</div>
             </div>
-        </v-col> 
+            </v-col>
         </div>
       </div>
       <div class="AboutMember">
         <div class="mt-5">
         <v-col align="center">
-          <div class="text-h5 font-weight-bold">Member</div>
-            <div class="my-10">
-              <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
-              <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
-              <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
+          <div class="text-h4 font-weight-bold">Member</div>
+            <div class="my-10 text-h5">
+              <div class="my-auto">
+              <v-avatar
+                color="primary"
+                size="99"
+              ></v-avatar>
+              <v-avatar
+                color="primary"
+                size="99"
+              ></v-avatar>
+              <v-avatar
+                color="primary"
+                size="99"
+              ></v-avatar>
+              <v-avatar
+                color="primary"
+                size="99"
+              ></v-avatar>
+              </div>
             </div>
         </v-col>  
         </div>
