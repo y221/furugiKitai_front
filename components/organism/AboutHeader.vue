1c5378674bf7f9acb7a1aa96e7475efd6b3ae89b
pmhr18 <tmh6.pm18@gmail.com>
2021-05-05 12:56:05 +0900

    テストと勉強も兼ねて一旦コミット

--- components/organism/AboutHeader.vue
+++ components/organism/AboutHeader.vue
@@ -2,42 +2,50 @@
   <div class="main-wrapper">
     <div class="main-content">
       <div class="FurugiKitaiLogo">
+        <div class="my-12">
         <v-col align="center">
           <FurugiKitaiLogo
             :height="height"
             :width="width"
           />
         </v-col>
+        </div>
       </div> 
       <div class="AboutAbout">
+        <div class="mt-5">
         <v-col align="center">
           <div class="text-h5 font-weight-bold">古着屋が見つかる検索サイト</div>
-            <div class="mt-5">
+            <div class="my-10">
               <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
               <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
               <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
             </div>
         </v-col>  
+        </div>
       </div>
       <div class="AboutConcept">
+        <div class="mt-5">
         <v-col align="center">
           <div class="text-h5 font-weight-bold">フルギキタイへの想い</div>
-            <div class="mt-5">
+            <div class="my-10">
               <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
               <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
               <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
             </div>
-        </v-col>  
+        </v-col> 
+        </div>
       </div>
       <div class="AboutMember">
+        <div class="mt-5">
         <v-col align="center">
           <div class="text-h5 font-weight-bold">Member</div>
-            <div class="mt-5">
+            <div class="my-10">
               <div>このサイトは運営者が古着をもっと知りたいという思いでつくりはじめたサイトです。</div>
               <div>全国の古着屋をデータベース化して、自分のお気に入りの古着屋をもっとたくさん見つけられたらという思い出つくりました。</div>
               <div>このサイトを共有することで古着を好きな人がより古着を楽しめるように、古着を知らない人が古着の良さを知れるようなサイトにしていきたいと考えています。</div>
             </div>
         </v-col>  
+        </div>
       </div>
     </div>
   </div> 
