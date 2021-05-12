acbaac3c913ac3e0fd51e60f3f371ce2674889ae
pmhr18 <tmh6.pm18@gmail.com>
2021-05-12 20:05:19 +0900

    UserPolicy修正途中だけどいったんプッシュ

---
+++ components/molecules/PolicyTitleBlock.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="d-flex align-center flex-row mb-6">
+    <v-sheet
+      class="mr-2"
+      color="#9BD4CE"
+      height="25"
+      rounded
+      width="25"
+    ></v-sheet>
+    <div :class="subHeader">第1条（適用）</div>
+  </div>
+</template>
