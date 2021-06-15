d6df08a9077e9742923eee20a1edefe891c4ccc6
pmhr18 <tmh6.pm18@gmail.com>
2021-06-15 23:24:50 +0900

    修正　モバイル対応はOK　デスクトップ表示のテキストエリアが不具合

---
+++ components/molecules/RequiredMark.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-sheet
+    color="accent"
+    rounded
+    height="20"
+    width="35"
+  >
+    <div class="white--text">
+      必須
+    </div>
+  </v-sheet>  
+</template>
--- components/organism/ContactForm.vue
+++ components/organism/ContactForm.vue
@@ -4,47 +4,42 @@
       <div class="main-wrapper">
         <div :class="mainContent">
           <v-col>
-          <div :class="title">
-            お問い合わせ
-          </div>
-          <div :class="description">
-            下記フォームを入力し、プライバシーポリシーをご確認の上「送信」ボタンを押してください。運営の都合上、返信には数日〜数週間などお時間を頂く場合がございますので、予めご了承ください。
-          </div>
-          <v-row>
-            <v-col :class="fieldsName" class="column" cols="2">
-              <div>
-                名前
-              </div>
-            </v-col>
-            <v-col cols="1">
-              <v-sheet
-                color="accent"
-                rounded
-                height="50"
-                width="100"
-              >
-                  <div :class="required" text-color="white">
-                    必須
+            <div :class="title">
+              お問い合わせ
+            </div>
+            <div :class="description">
+              下記フォームを入力し、プライバシーポリシーをご確認の上「送信」ボタンを押してください。運営の都合上、返信には数日〜数週間などお時間を頂く場合がございますので、予めご了承ください。
+            </div>
+              <v-row class="formLine">
+                <v-col
+                  cols="auto"
+                  class="d-flex align-center"
+                  :class="fieldsName"
+                >
+                  <div>
+                    メールアドレス
                   </div>
-              </v-sheet>
-            </v-col>
-            <v-col>
-              <v-text-field
-                placeholder="例：フルギ　キタイ"
-                outlined
-              ></v-text-field>
-            </v-col>
-          </v-row>
+                </v-col>
+                <v-col
+                  cols="auto"
+                  class="d-flex align-center"
+                  :class="required"
+                >
+                  <RequiredMark />
+                </v-col>
+                <v-col
+                  cols="12"
+                  md="auto"
+                  :class="textField"
+                >
+                  <v-text-field
+                    placeholder="例：フルギ　キタイ"
+                    outlined
+                    class="text-body-2"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
             <v-divider></v-divider>
-
-          <v-row>
-          </v-row>
-          <v-row>
-          </v-row>
-          <v-row>
-          </v-row>
-          <v-row>
-          </v-row>
           </v-col>
         </div>
       </div>
@@ -56,7 +51,7 @@
 export default {
   computed: {
     mainContent () {
-      if (this.$vuetify.breakpoint.xs) return 'main-content mt-4'
+      if (this.$vuetify.breakpoint.xs) return 'main-content mt-4 pb-8'
       return 'main-content mt-6 mx-12'
     },
     title () {
@@ -67,18 +62,22 @@ export default {
       if (this.$vuetify.breakpoint.xs) return 'text-body-2 mx-2 mb-10'
       return 'text-body-1 mx-4 mb-12'
     },
+    formLine () {
+      if (this.$vuetify.breakpoint.xs) return 'px-1 mt-3'
+      return 'mx-2 pt-3'
+    },
     fieldsName () {
-      if (this.$vuetify.breakpoint.xs) return 'text-body-2 mx-2 mb-10'
+      if (this.$vuetify.breakpoint.xs) return 'text-button font-weight-bold px-2 ml-1'
       return 'text-body-2 font-weight-bold'
     },
     required () {
-      if (this.$vuetify.breakpoint.xs) return 'text-body-2 mx-2 mb-10'
-      return 'text-caption'
+      if (this.$vuetify.breakpoint.xs) return 'text-caption text-center mx-n4'
+      return 'text-caption text-center '
     },
-    column () {
-      if (this.$vuetify.breakpoint.xs) return 'text-body-2 mx-2 mb-10'
-      return 'align-justify'
-    }    
+    textField () {
+      if (this.$vuetify.breakpoint.xs) return 'mt-n6 mb-n3'
+      return 'mt-5 mb-n3'
+    }
   }
 }
 </script>
