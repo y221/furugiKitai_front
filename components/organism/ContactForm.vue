4598f57f2bba9e87b0c70cb5107ebe8e6295bf43
pmhr18 <tmh6.pm18@gmail.com>
2021-06-16 22:46:14 +0900

    プライバシーポリシーに同意するチェックボックスの v-row 以外実装OK　休憩

--- components/organism/ContactForm.vue
+++ components/organism/ContactForm.vue
@@ -8,20 +8,21 @@
               お問い合わせ
             </div>
             <div :class="description">
-              下記フォームを入力し、プライバシーポリシーをご確認の上「送信」ボタンを押してください。運営の都合上、返信には数日〜数週間などお時間を頂く場合がございますので、予めご了承ください。
+              下記フォームを入力し、プライバシーポリシーをご確認の上「送信する」ボタンを押してください。運営の都合上、返信には数日から数週間などお時間を頂く場合がございますので、予めご了承ください。
             </div>
-              <v-row class="formLine">
+              <v-row :class="formLine">
                 <v-col
                   cols="auto"
+                  lg="2"
                   class="d-flex align-center"
                   :class="fieldsName"
                 >
                   <div>
-                    メールアドレス
+                    名前
                   </div>
                 </v-col>
                 <v-col
-                  cols="auto"
+                  lg="1"
                   class="d-flex align-center"
                   :class="required"
                 >
@@ -29,7 +30,7 @@
                 </v-col>
                 <v-col
                   cols="12"
-                  md="auto"
+                  lg="9"
                   :class="textField"
                 >
                   <v-text-field
@@ -39,7 +40,96 @@
                   ></v-text-field>
                 </v-col>
               </v-row>
-            <v-divider></v-divider>
+            <v-divider class="mx-auto" :width="dividerWidth"></v-divider>
+              <v-row :class="formLine">
+                <v-col
+                  cols="auto"
+                  lg="2"
+                  class="d-flex align-center"
+                  :class="fieldsName"
+                >
+                  <div>
+                    メールアドレス
+                  </div>
+                </v-col>
+                <v-col
+                  lg="1"
+                  class="d-flex align-center"
+                  :class="required"
+                >
+                  <RequiredMark />
+                </v-col>
+                <v-col
+                  cols="12"
+                  lg="9"
+                  :class="textField"
+                >
+                  <v-text-field
+                    placeholder="例：info@furugikitai.com"
+                    outlined
+                    class="text-body-2"
+                  ></v-text-field>
+                </v-col>
+              </v-row>
+            <v-divider class="mx-auto" :width="dividerWidth"></v-divider>
+              <v-row :class="formLine">
+                <v-col
+                  cols="auto"
+                  lg="2"
+                  class="d-flex align-center"
+                  :class="fieldsName"
+                >
+                  <div>
+                    お問い合わせ内容
+                  </div>
+                </v-col>
+                <v-col
+                  lg="1"
+                  class="d-flex align-center"
+                  :class="required"
+                >
+                  <RequiredMark />
+                </v-col>
+                <v-col
+                  cols="12"
+                  lg="9"
+                  :class="textField"
+                >
+                  <v-textarea
+                    placeholder="例：お問い合わせ内容を入力してください"
+                    outlined
+                    class="text-body-2"
+                  ></v-textarea>
+                </v-col>
+              </v-row>
+            <v-divider class="mx-auto" :width="dividerWidth"></v-divider>
+
+              <v-row :class="formLine">
+                <v-col
+                  cols="1"
+                  :class="checkBox"  
+                >
+                  <v-checkbox
+                  ></v-checkbox>
+                </v-col>
+                <v-col>
+                  <div :class="agreeText">
+                    プライバシーポリシーに同意する
+                  </div>
+                </v-col>
+              </v-row>
+
+              <v-row :justify="submitButtonJustify">
+                <v-btn
+                  color="primary"
+                  depressed
+                  rounded
+                  :class="submitButtonPosition"
+                  :width="submitButtonWidth"
+                >
+                  送信する
+                </v-btn>                
+              </v-row>
           </v-col>
         </div>
       </div>
@@ -63,21 +153,49 @@ export default {
       return 'text-body-1 mx-4 mb-12'
     },
     formLine () {
-      if (this.$vuetify.breakpoint.xs) return 'px-1 mt-3'
-      return 'mx-2 pt-3'
+      if (this.$vuetify.breakpoint.mdAndDown) return 'px-1 mt-4'
+      return 'mx-1 pt-3'
+    },
+    dividerWidth () {
+      if (this.$vuetify.breakpoint.mdAndDown) return '100%'
+      return '95%'
     },
     fieldsName () {
-      if (this.$vuetify.breakpoint.xs) return 'text-button font-weight-bold px-2 ml-1'
-      return 'text-body-2 font-weight-bold'
+      if (this.$vuetify.breakpoint.mdAndDown) return 'text-button font-weight-bold px-2 ml-1'
+      return 'ml-3 text-body-2 font-weight-bold'
     },
     required () {
-      if (this.$vuetify.breakpoint.xs) return 'text-caption text-center mx-n4'
-      return 'text-caption text-center '
+      if (this.$vuetify.breakpoint.mdAndDown) return 'text-caption text-center mx-n4'
+      return 'text-caption text-center'
     },
     textField () {
-      if (this.$vuetify.breakpoint.xs) return 'mt-n6 mb-n3'
-      return 'mt-5 mb-n3'
-    }
+      if (this.$vuetify.breakpoint.mdAndDown) return 'mt-n5 mb-n1'
+      return 'mx-n8 mt-3 mb-n3'
+    },
+    checkBoxLine () {
+      if (this.$vuetify.breakpoint.mdAndDown) return 'mt-5 mb-5'
+      return ''      
+    },  
+    checkBox () {
+      if (this.$vuetify.breakpoint.mdAndDown) return 'mt-n5 mb-n1'
+      return ''      
+    },
+    agreeText () {
+      if (this.$vuetify.breakpoint.mdAndDown) return 'text-button'
+      return 'text-body-2' 
+    },
+    submitButtonJustify () {
+      if (this.$vuetify.breakpoint.mdAndDown) return 'center'
+      return 'end'        
+    },
+    submitButtonWidth () {
+      if (this.$vuetify.breakpoint.mdAndDown) return '95%'
+      return '645px'        
+    },
+    submitButtonPosition () {
+      if (this.$vuetify.breakpoint.mdAndDown) return 'mb-10'
+      return 'mr-12 my-6 mb-12'
+    } 
   }
 }
 </script>
