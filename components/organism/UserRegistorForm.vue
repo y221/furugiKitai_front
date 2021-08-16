a0c82a5c05bc59ee07ed102184985ad5c08cbf81
yuta_tsutsui <ydsikohsh@gmail.com>
2021-08-16 23:50:44 +0900

    インデント修正

--- components/organism/UserRegistorForm.vue
+++ components/organism/UserRegistorForm.vue
@@ -4,25 +4,25 @@
       <div :class="header">
         ユーザー情報登録
       </div>
-        <div>
-          <FormUserIcon imagePath="" imageName="ユーザーアイコン" />
-          <FormTextfield columnName="名前" label="例：古着大好きおじさん" :required="true" id="name" v-on:change="changeValue"/>
-          <FormTextarea columnName="好きな古着" label="例：ビンテージのデニム" :required="false" id="favorite"  v-on:change="changeValue"/>
-          <FormTextarea columnName="プロフィール" label="例：ホゲホゲ" :required="false" id="profile"  v-on:change="changeValue"/>
-          <FormTextfield columnName="Instagram" label="例：https://www.instagram.com/xxxxx/" :required="false" id="instagram"  v-on:change="changeValue"/>
-        </div>
-        <v-col align="center">
-          <v-btn
-            class="mx-auto my-6 font-weight-bold"
-            color="primary"
-            depressed
-            rounded
-            width="200px"
-            @click="registerUsers()"
-          >
-            登録
-          </v-btn>
-        </v-col>
+      <div>
+        <FormUserIcon imagePath="" imageName="ユーザーアイコン" />
+        <FormTextfield columnName="名前" label="例：古着大好きおじさん" :required="true" id="name" v-on:change="changeValue"/>
+        <FormTextarea columnName="好きな古着" label="例：ビンテージのデニム" :required="false" id="favorite"  v-on:change="changeValue"/>
+        <FormTextarea columnName="プロフィール" label="例：ホゲホゲ" :required="false" id="profile"  v-on:change="changeValue"/>
+        <FormTextfield columnName="Instagram" label="例：https://www.instagram.com/xxxxx/" :required="false" id="instagram"  v-on:change="changeValue"/>
+      </div>
+      <v-col align="center">
+        <v-btn
+          class="mx-auto my-6 font-weight-bold"
+          color="primary"
+          depressed
+          rounded
+          width="200px"
+          @click="registerUsers()"
+        >
+          登録
+        </v-btn>
+      </v-col>
     </div>
   </div>
 </template>
