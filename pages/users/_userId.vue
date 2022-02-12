a1099abf2b80cbcd958a98bdf33fdad7f126f745
hidetsugu-o <74892038+hidetsugu-o@users.noreply.github.com>
2022-02-12 12:46:03 +0900

    データ取得（仮）

--- pages/users/_userId.vue
+++ pages/users/_userId.vue
@@ -1,17 +1,32 @@
 <template>
-	<div>
-		<Header :isTop="false" />
-		<v-main class="main-background-color display">
-			<v-container>
-				<UserDetail />
-			</v-container>
-		</v-main>
-		<Footer />
-	</div>
+  <div>
+    <Header :isTop="false" />
+    {{user}}
+    <v-main class="main-background-color display">
+      <v-container>
+        <UserDetail />
+      </v-container>
+    </v-main>
+    <Footer />
+  </div>
 </template>
+
+<script>
+
+export default {
+  data: () => ({
+    user: []
+  }),
+  async created() {
+    console.log(this.$route.params.userId)
+    const response = await this.$axios.get(`/api/api/users/`+this.$route.params.userId);
+    this.user = response.data;
+  }
+}
+</script>
 
 <style>
 .display {
-	min-height: 100vh;
+  min-height: 100vh;
 }
 </style>
