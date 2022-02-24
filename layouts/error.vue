08fe4190ace9aee7be27eaf5e9d7293b3b2b9b59
pmhr18 <tmh6.pm18@gmail.com>
2022-02-24 13:57:11 +0900

    ベース作成

---
+++ layouts/error.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="fill-height">
+    <Header :isTop="false" />
+    <v-main class="main-background-color">
+      <v-container>
+      <div class="ErrorPage">
+        <div class="ErrorPage__content">
+          <div class="ErrorPage__inner">
+            <template v-if="error.statusCode === 404">
+
+              <v-img
+                alt="shoot"
+                :height="479"
+                :width="312"
+                src="/images/404.png"
+              >
+              </v-img>
+
+              <span>ERROR</span>
+              <h2>ページが見つかりません。</h2>
+              <p>URLに間違いがないかご確認ください。</p>
+              <v-btn
+                class="mx-auto my-6 font-weight-bold"
+                color="primary"
+                depressed
+                rounded
+                width="200px"
+                to="/"
+              >
+                トップページに戻る
+              </v-btn>
+            </template>
+          </div>
+        </div>
+      </div>
+      </v-container>
+    </v-main>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
