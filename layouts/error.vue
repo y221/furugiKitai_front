4bf3bdb72724a0e9b65e8abc23fff44d49799743
pmhr18 <tmh6.pm18@gmail.com>
2022-02-24 14:49:51 +0900

    微修正

--- layouts/error.vue
+++ layouts/error.vue
@@ -7,18 +7,18 @@
         <div class="ErrorPage__content">
           <div class="ErrorPage__inner">
             <template v-if="error.statusCode === 404">
-
               <v-img
                 class="mt-8 mb-12 mx-auto"
                 alt="shoot"
-                :max-height="this.$vuetify.breakpoint.xs ? '192' : '406'"
-                :max-width="this.$vuetify.breakpoint.xs ? '127' : '269'"
+                :max-height="this.$vuetify.breakpoint.xs ? '225' : '406'"
+                :max-width="this.$vuetify.breakpoint.xs ? '149' : '269'"
                 src="/images/404.png"
               >
               </v-img>
-
-              <div class="text-h1 font-weight-bold">404</div>
-              <div class="text-h2 font-weight-medium">PAGE NOT FOUND</div>
+              <div class="grey--text text--darken-3">
+                <div :class="this.$vuetify.breakpoint.xs ? 'text-h2 font-weight-bold' : 'text-h1 font-weight-bold'">404</div>
+                <div :class="this.$vuetify.breakpoint.xs ? 'text-h4 font-weight-bold' : 'text-h2 font-weight-bold'">PAGE NOT FOUND</div>
+              </div>
               <p :class="this.$vuetify.breakpoint.xs ? 'text-caption mt-4' : 'text-body-1 mt-4'"
               >
                 お探しのページは見つかりませんでした。<br v-if="this.$vuetify.breakpoint.xs">移動または削除された可能性があります。
