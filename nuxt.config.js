03ba06f07c9e03ec2122d80e16851c26e38080a4
筒井雄太 <yuta_tsutsui@tsutsuiyuutanoMacBook-Air.local>
2022-04-08 23:21:25 +0900

    修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'フルギキタイ',
-    titleTemplate: 'フルギキタイ | %s',
+    titleTemplate: '%s | フルギキタイ ',
     htmlAttrs:{
       lang: 'ja'
     },
